a46907fc40f3c903ed0dee37fa28adff2822d90f
noam106 <117109080+noam106@users.noreply.github.com>
2023-02-01 17:57:18 +0200

    from noam

---
+++ library_backend/book_code.py
@@ -0,0 +1,77 @@
+import datetime
+import re
+
+import library_test.useful_function
+from library_test import *
+
+
+class Book:
+    def __init__(self, book_id: str, book_name: str, author: dict, year_publish: datetime, type_of_loan: int):
+        self._type_of_loan = type_of_loan
+        self._year_publish = datetime.datetime.strptime(year_publish, '%y')
+        self._author = author
+        self._book_name = book_name
+        self._book_id = book_id
+        if self._type_of_loan not in (1, 2, 3):
+            raise  #write a raise condition
+        if self._type_of_loan == 1:
+            self._max_time_loan = datetime.timedelta(days=10)
+        elif self._type_of_loan == 2:
+            self._max_time_loan = datetime.timedelta(days=5)
+        else:
+            self._max_time_loan = datetime.timedelta(days=2)
+
+    def get_type_of_loan(self):
+        return self._type_of_loan
+
+    def set_type_of_loan(self, new_type: int):
+        self._type_of_loan = new_type
+
+    def get_year_published(self):
+        return self._year_publish
+
+    def get_author(self):
+        return self._author
+
+    def get_book_name(self):
+        return self._book_name
+
+    def get_book_id(self):
+        return self._book_id
+
+
+class Customer:
+    def __init__(self, customer_id: int, customer_name: dict, address: classmethod, email: str, birth_day: datetime):
+        self._birth_day = birth_day
+        self._email = email
+        self._address = address
+        self._customer_name = customer_name
+        self._customer_id = customer_id
+
+    def get_birth_day(self):
+        return self._birth_day
+
+    def get_email(self):
+        return self._email
+
+    def set_email(self, new_email: str):
+        if library_test.useful_function.isvalid_email(new_email):
+            self._email = new_email
+
+    def get_address(self):
+        return self._address
+
+    def get_customer_name(self):
+        return self._customer_name
+
+    def get_customer_id(self):
+        return self._customer_id
+
+
+class Loan:
+    def __init__(self, costumer_id: int, book_id: int):
+        self._costumer_id = costumer_id
+        self._book_id = Book.get_book_id()
+
+
+
---
+++ library_test/useful_function.py
@@ -0,0 +1,63 @@
+import re
+import datetime
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+
+def isvalid_email(email) -> bool:
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
+def time_loan_2_max_days(type_loan: int) -> datetime:
+    if type_loan not in (1, 2, 3):
+        raise  # write a raise condition
+    if type_loan == 1:
+        max_time_loan = datetime.timedelta(days=10)
+    elif type_loan == 2:
+        max_time_loan = datetime.timedelta(days=5)
+    else:
+        max_time_loan = datetime.timedelta(days=2)
+    return max_time_loan
+
+
+def is_valid_name(name_str: str) -> bool:
+    if re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,25})?', name_str):
+        return True
+    else:
+        return False
+
+
+class Address:
+
+    def __init__(self, street: str, city: str, zipcode: str, house_num: str):
+        self._street = street
+        self._city = city
+        self._house_num = house_num
+        self._zipcode = zipcode
+
+    def get_street(self):
+        return self._street
+
+    def get_city(self):
+        return self._city
+
+    def get_house_num(self):
+        return self._house_num
+
+    def get_zipcode(self):
+        return self._zipcode
+
+
+def is_valid_bday(user_bday: str) -> str:
+    if re.search(
+        "^([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(\.|-|/)([1-9]|0[1-9]|1[0-2])(\.|-|/)"
+        "([0-9][0-9]|19[0-9][0-9]|20[0-9][0-9])$|^([0-9][0-9]|19[0-9][0-9]|20[0-9]"
+        "[0-9])(\.|-|/)([1-9]|0[1-9]|1[0-2])(\.|-|/)([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])$",
+        '01/01/2018'):
+        return True
+    else:
+        False
+print(is_valid_bday('1.5.99'))
