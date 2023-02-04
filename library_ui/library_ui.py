44f32e3973c4fabec33e465206fe3489f43bc2a5
noam106 <117109080+noam106@users.noreply.github.com>
2023-02-04 16:59:31 +0200

    Shabat Shalom

--- library_ui/library_ui.py
+++ library_ui/library_ui.py
@@ -2,3 +2,17 @@ import datetime
 
 if __name__ == '__main__':
     print('hello there, before we start')
+    while True:
+        print('In any time you wish to leave the program insert: "#"')
+        user_choice = input('What would you like to do?\n'
+                            'To add a new customer or new book insert "1"\n'
+                            'To Loan or return a book insert "2"\n'
+                            'To display information of loans, books, or customer insert "3"\n'
+                            'To remove a book or customer from library insert "4"\n'
+                            'To find a book insert "5"\n'
+                            'To find a customer insert "6"\n'
+                            'Insert your choice here:\n')
+        if user_choice not in ('1', '2', '3', '4', '5', '6', '#'):
+            print('your choice is invalid, please try again: \n')
+        else:
+            break
